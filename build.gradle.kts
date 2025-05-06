4398613acc4aa2808b0098d13f32c98980df2bcb
aviv926 <51673860+aviv926@users.noreply.github.com>
2025-05-06 14:38:41 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,23 @@
-android {
-    // ... other configurations (compileSdk, defaultConfig, etc.)
+// android {
+    // ... other configurations
 
+    // You can remove or comment out the entire signingConfigs block
+    // if it's only used for CI signing and you'll sign locally.
+    /*
     signingConfigs {
-        release {
-            if (System.getenv("RELEASE_STORE_FILE_PATH") != null) {
-                storeFile file(System.getenv("RELEASE_STORE_FILE_PATH"))
-                storePassword System.getenv("RELEASE_KEYSTORE_PASSWORD")
-                keyAlias System.getenv("RELEASE_KEY_ALIAS")
-                keyPassword System.getenv("RELEASE_KEY_PASSWORD")
-            } else {
-                // Optional: Configuration for local builds, e.g., using a local keystore.properties
-                // Make sure keystore.properties is in .gitignore
-                // storeFile file("../keystore.properties")
-                // storePassword ...
-                // keyAlias ...
-                // keyPassword ...
-                println("Warning: Release signing information not found in environment variables.")
-            }
+        create("release") {
+            // Details for signing, which we will skip in CI
         }
     }
+    */
 
-    buildTypes {
-        release {
-            signingConfig signingConfigs.release
-            // ... other release build type configurations (e.g., minifyEnabled, proguardFiles)
-        }
-    }
-    // ...
-}
+//    buildTypes {
+//        getByName("release") {
+//            isMinifyEnabled = false
+//            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            // Ensure this line is removed or commented out if you don't want CI to sign:
+            // signingConfig = signingConfigs.getByName("release")
+//        }
+//    }
+// ...
+// }
